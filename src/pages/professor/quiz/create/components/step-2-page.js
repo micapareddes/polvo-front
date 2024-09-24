e3ddb132984787ced1cc4bd44e1d90405033d185
micapareddes <micapareddes@gmail.com>
2024-09-24 14:33:58 -0300

    testes para resolver bug de criacao de quiz

--- src/pages/professor/quiz/create/components/step-2-page.js
+++ src/pages/professor/quiz/create/components/step-2-page.js
@@ -285,8 +285,14 @@ export async function Step2Page() {
     const perguntas = perguntasRascunho || perguntasAlteradas
     if (perguntas) {
         const perguntasContainer = form.querySelectorAll('.pergunta-container')
+        console.log('perguntas container: ',perguntasContainer); 
+
         perguntasContainer.forEach((container) => {
+            console.log('pergunta container: ',container); 
+            
             const pergunta = container.querySelector('.pergunta')
+            console.log('pergunta', pergunta);
+            
             const correta = container.querySelector('.correta')
             const incorretas = container.querySelectorAll('.incorreta')
             const incorreta1 = incorretas[0]
