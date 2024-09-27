54a8f10e8b589b14ce7267afb76fe348fb740497
micapareddes <micapareddes@gmail.com>
2024-09-27 15:09:58 -0300

    Resolve validacao de email

--- src/validations/cadastroUserValidation.js
+++ src/validations/cadastroUserValidation.js
@@ -1,6 +1,6 @@
 export function cadastroUserValidation(data) {
     const name = data.nome.length >= 3
-    const email = data.email.includes('@') //TODO: melhorar validação
+    const email = data.email.includes('@') && data.email.includes('.com')//TODO: melhorar validação
     const matricula = data.matricula.length === 6 //TODO: validar que seja numeros
     return {
         success: name && email && matricula,
